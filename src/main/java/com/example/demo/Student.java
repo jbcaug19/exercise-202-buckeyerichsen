f2bd5e9fb23f438832dfbc403fb11c8346478a14
buckeyerichsen <buckey.erichsen@gmail.com>
2019-08-28 08:41:03 -0400

    202

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+    private int height;
+    private String cellphone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
